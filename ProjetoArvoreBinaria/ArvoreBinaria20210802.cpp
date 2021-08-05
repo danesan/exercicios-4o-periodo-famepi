bfa7d46dd11a8ba8848fead4cc6e3e841d863232
Daniel Santos <danesansilva@gmail.com>
2021-08-04 20:12:53 -0400

    Implementando o método de menu, preencher_item, inserir, mostrar_arvore

--- ProjetoArvoreBinaria/ArvoreBinaria20210802.cpp
+++ ProjetoArvoreBinaria/ArvoreBinaria20210802.cpp
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <locale.h>
-#include <system.h>
+#include <stdlib.h>
+#include <conio.h>
 
 typedef struct tipo_item {
 	int cod;
@@ -15,24 +16,116 @@ typedef struct tipo_no {
 	tipo_no *dir;
 };
 
-inserir() {
+tipo_item preencher_item() {
+	tipo_item item;
+	printf("\n\n ***** PREENCHENDO ITEM A SER INSERIDO *****");
+	printf("\n Digite o código: ");
+	scanf("%d", &item.cod);
+	fflush(stdin);
+	printf("\n Digite o nome do item: ");
+	gets(item.nome);
+	printf("\n Digite a quantidade: ");
+	scanf("%d", &item.quant);
+	
+	return (item);
+}
+
+tipo_no* inserir(tipo_item aux, tipo_no *raiz, tipo_no *r) {
+	
+	if(!r) { //se não estiver apontando para algum endereço de memória
+		r = (tipo_no *) malloc(sizeof(tipo_no));
+		
+		if(!r) {
+			printf("\n\n ERRO: problema de espeço de memória!");
+			getch();
+			exit(0);
+		}
+		
+		r->esq = NULL;
+		r->dir = NULL;
+		r->item = aux;
+		
+		if(!raiz) {
+			printf("\n\n Item inserido com sucesso!");
+			getch();
+			return r;   //primeiro entrada => raiz da árvore
+		}
+		
+		if(aux.cod < raiz->item.cod) {
+			raiz->esq = r;
+		} else {
+			raiz->dir = r;
+		}
+		
+		printf("\n\n Item inserido com sucesso!");
+		getch();
+		return r;
+	}
+	
+	if(aux.cod < r->item.cod) {
+		inserir(aux, r, r->esq); // aqui entra a recursão para o filho da esquerdsa
+	} else if(aux.cod > r->item.cod) {
+		inserir(aux, r, r->dir); //aqui entra a recursão para o filho da direita
+	} else {
+		printf("\n\n IMPOSSÍVEL INSERIR!");
+		printf("\n\n Motivo: Já existe um item com o código %d na árvore", aux.cod);
+		getch();
+	}
+}
+
+void mostrar(tipo_no *raiz) {
+	
+	if(!raiz) {
+		return;
+	}
+	
+	mostrar(raiz->esq);
+	printf("\n**********************");
+	printf("\n Cod: %d", raiz->item.cod);	
+	printf("\n Nome: %s", raiz->item.nome);
+	printf("\n Quantidade: %d \n", raiz->item.quant);
+	mostrar(raiz->dir);
+}
+
+void pesquisar() {
 	
 }
 
-mostrar() {
+void excluir() {
+}
+
+void mostrarItem() {
 	
 }
 
-pesquisar() {
+int menu() {
+	int op = 0;
 	
+	do {
+		printf("\n\n ********** MENU **********");
+		printf("\n [1] - INSERIR");
+		printf("\n [2] - MOSTRAR");
+		printf("\n [3] - ");
+		printf("\n [4] - ");
+		printf("\n [5] - ");
+		printf("\n [0] - SAIR");
+		
+		printf("\n\n Opção: ");
+		scanf("%d", &op);
+		
+		if(op == 0) {
+			exit(0);
+		} else if(op < 1 || op > 5) {
+			printf("\nOPÇÃO INVÁLIDA!");
+			getch();
+			system("cls");
+		} 
+		return op;
+	} while (true);
 }
 
-excluir() {
-}
 
-mostrarItem() {
-	
-}
+
 
 int main() {
 	setlocale(LC_ALL,"");
@@ -51,13 +144,18 @@ int main() {
 				getch();
 				return 0;
 			case 1:
-				aux = preencher _item();
+				aux = preencher_item();
 				if(!raiz) 
 					raiz = inserir(aux, raiz, raiz);
 				else 
 					inserir(aux, raiz, raiz);
 				break;
 			case 2: 
+				if(!raiz) {
+					printf("\nÁrvore vazia! Insira uma item primeiro.");
+					getch();
+					break;
+				}
 				printf("\n\n\CONTEÚDO DA ÁRVORE: ");
 				mostrar(raiz);
 				getch();
@@ -67,7 +165,7 @@ int main() {
 				printf("\n\n\Digite o código do item a pesquisar: ");
 				scanf("%d", &cod);
 				achou = 0;
-				pesquisar(&achou, cod, raiz);
+				//pesquisar(&achou, cod, raiz);
 				if(!achou) {
 					printf("\n\nO item de código %d não está na árvore", cod);
 				}
@@ -78,7 +176,7 @@ int main() {
 				printf("\n\n\Digite o código do item a excluir: ");
 				scanf("%d", &cod);
 				achou = 0;
-				raiz = excluir(&achou, cod, raiz);
+				//raiz = excluir(&achou, cod, raiz);
 				if(!achou) {
 					printf("\n\nO item de código %d não está na árvore", cod);
 				}
@@ -86,5 +184,6 @@ int main() {
 				break;
 
 		}
-	}
+	} while (true);
+	return 0;
 }
